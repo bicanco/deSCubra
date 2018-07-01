3145ebfc7cc45d3f1b9ad1de50c1c052b76b7f29
Gabriel Toschi <gabriel.toschi@hotmail.com>
2018-07-01 16:15:23 -0300

    new app

--- client/src/App.js
+++ client/src/App.js
@@ -67,7 +67,6 @@ class App extends Component {
           <TopMenu isAdmin={true} />
         </header>
         <main className="App">
-          <LoginAdmin />
         </main>
         <footer>
           <FootMenu />
