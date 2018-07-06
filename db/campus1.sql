939d8d391c4087b3b4adce53e6efeab5171dde4d
Gabriel Toschi <gabriel.toschi@hotmail.com>
2018-07-06 02:13:06 -0300

    percurso campus 1 no db

---
+++ db/campus1.sql
@@ -0,0 +1,118 @@
+INSERT INTO PERCURSO VALUES (
+	'USP - Campus 1',
+	'Fundada em 1948,  a USP São Carlos teve início com a Escola de Engenharia de São Carlos (EESC), onde hoje se encontra o Centro de Divulgação Científica e Cultural (CDCC). Com a crescente demanda de novos alunos, foi necessário a transferência para um terreno maior, onde se encontra o atual Campus 1. Conheça mais sobre o campus neste percurso!',
+	'ph.jpg');
+
+INSERT INTO PARADA VALUES ('USP - Campus 1', 0,
+	'Laboratório de Madeiras e Estruturas de Madeiras (LaMEM)',
+	'O Laboratório de Madeiras e de Estruturas de Madeira (LaMEM) realiza pesquisas teóricas e experimentais em madeiras e estruturas de madeira e prestação de serviços para a comunidade na área de controle de qualidade de madeiras e derivados, normalização e recomendações de projeto e construção de estruturas de madeira.',
+	'Suba a rua em direção aos prédios de madeira. Acham que eu sou miojo, mas qual é meu nome de verdade?',
+	'laboratório de madeiras e estruturas de madeiras;lamem'
+	,'ph.jpg');
+
+INSERT INTO PARADA VALUES ('USP - Campus 1', 1,
+	'Instituto de Arquitetura e Urbanismo (IAU)',
+	'O Instituto de Arquitetura e Urbanismo da Universidade de São Paulo (ou simplesmente IAU) é uma unidade da Universidade de São Paulo responsável pelo ensino de graduação, pós-graduação, pesquisa e extensão de serviços à sociedade na área de Arquitetura e Urbanismo e nos campos de conhecimento que lhes são correlatos, como o Design, o Paisagismo entre outros, no campus de São Carlos. Criado em 2010, o instituto é um resultado da consolidação do curso, o qual promove a reflexão sobre o lugar do arquiteto e urbanista em um período de intensa transformação social, cultural, tecnológica e econômicas.',
+	'Atravesse a rua e siga reto até a próxima rua. Olhe em volta! Os alunos desse instituto adoram cozinhar juntos… Qual deve ser a comida preferida deles?',
+	'pizza'
+	,'ph.jpg');
+
+INSERT INTO PARADA VALUES ('USP - Campus 1', 2,
+	'Prefeitura do Campus',
+	'A Prefeitura é a entidade que administra o campus, visando a manutenção de infraestrutura e serviços comuns no campus, necessários para o bom desenvolvimento das atividades de ensino, pesquisa e extensão da Universidade. Ela responde à reitoria da USP.',
+	'Vire à direita e suba a rua. Toda cidade tem um ― e esse campus também tem um!',
+	'prefeitura;prefeitura do campus;prefeitura campus'
+	,'ph.jpg');
+
+INSERT INTO PARADA VALUES ('USP - Campus 1', 3,
+	'CEFER',
+	'A população do campus pode usufruir da infraestrutura e das atividades oferecidas pelo Centro de Educação Física, Esportes e Recreação (CEFER), da Prefeitura do Campus. São cinco quadras, duas piscinas, academia, campo de futebol, ginásio de esportes com quadra poliesportiva e capacidade para 1.300 pessoas e salão de eventos com 1.405 m². O grande evento esportivo promovido pelo Cefer é a Volta USP-São Carlos , uma prova pedestre que acontece anualmente pelas ruas da cidade e conta com a presença de toda a comunidade local e também de atletas de diversas partes do país.',
+	'Dê a volta no prédio e prossiga pelo estacionamento. Além dos universitários, crianças também brincam nessas areias. De que projeto elas participam?',
+	'pequeno cidadao;projeto pequeno cidadao'
+	,'ph.jpg');
+
+INSERT INTO PARADA VALUES ('USP - Campus 1', 4,
+	'Centro de Convivência da Química (CCQ/Centrinho)',
+	'O Centro de Convivência da Química (CCQ) é a secretaria acadêmica que representa os alunos do bacharelado em Química do Instituto de Química de São Carlos. Criado em 1999, o local, apelidado carinhosamente de “centrinho”, possui diversas finalidades, como reuniões de grupos de extensão, buscar materiais de veteranos que são deixados para ajudar os calouros, ou mesmo para relaxar entre as aulas, dormir no sofá e jogar um pebolim.',
+	'Atravesse o bosque e vá ato prédio estreito à sua esquerda. Sou um pouco lerdo, mas cheguei até aqui e virei o símbolo deste lugar. Quem eu sou?',
+	'caracol;lesma;caramujo'
+	,'ph.jpg');
+
+INSERT INTO PARADA VALUES ('USP - Campus 1', 5,
+	'Instituto de Química de São Carlos (IQSC)',
+	'O Instituto de Química de São Carlos (IQSC) foi criado em 1994, com a separação do antigo IFQSC. O IQSC é formado por dois departamentos: DFQ - Departamento de Físico-Química e DQFM - Departamento de Física e Química Molecular. O IQSC oferece dois cursos de graduação - Bacharelado em Química e Licenciatura em Ciências Exatas (interunidades) - e um programa de pós-graduação em Química com três áreas de concentração - Físico-Química, Química Analítica e Inorgânica e Química Orgânica e Biológica.',
+	'Vire no centro de cópias e siga reto até conseguir algo para se localizar. Sou a maior parte do ar que você respira. Como gás, sou livre; mas aqui me prenderam em um tanque. Quem sou eu?',
+	'nitrogenio;nitrogenio liquido;n2;n²'
+	,'ph.jpg');
+
+INSERT INTO PARADA VALUES ('USP - Campus 1', 6,
+	'Alojamento Estudantil',
+	'Administrado de forma democrática pelos próprios moradores, o alojamento estudantil da USP São Carlos possui cinco prédios destinados àqueles que dele necessitem para prosseguir os estudos. Todos os anos os moradores do alojamento acolhem os novos alunos como hóspedes, até que consigam uma república, imóvel ou até que o processo de seleção de moradores seja finalizado. Em São Carlos, a seleção para Alojamento e Bolsa Auxílio-Moradia é feita por uma comissão mista (Alunos, professores e assistentes sociais). Nos demais Campi da USP, a seleção é feita unicamente pela SAS (Superintendência de Assistência Social).',
+	'Siga em frente e olhe para o lado… direito. Assim como LEGO, sou dividido em blocos. Qual você não está vendo?',
+	'bloco A;A;blocoA;blocoa;a;bloco a'
+	,'ph.jpg');
+
+INSERT INTO PARADA VALUES ('USP - Campus 1', 7,
+	'Laboratórios de Ensino de Física (LEF-IFSC)',
+	'Tão antigo quanto o próprio campus de São Carlos da Universidade de São Paulo (USP), fundado em 1954, é o conjunto de Laboratórios de Ensino de Física (LEF), originado quase simultaneamente à inauguração do campus.  Desde a contratação dos primeiros docentes para ministrar disciplinas de Física, foi montada uma estrutura de laboratórios de ensino, em princípio localizada nas dependências da Escola de Engenharia, depois transferido ao prédio dos Departamentos (antigo IFQSC)- onde, hoje, se localiza o espaço da Biofísica-, posteriormente no Instituto de Química de São Carlos (IQSC) para, em 1988, estabelecer-se numa área que, atualmente, ocupa 2.674,96m².',
+	'Contorne o campo de futebol, vire na primeira rua à direita e siga reto. Cada uma das saídas do campus é apelidado pelo curso mais próximo. A sua direita, temos a saída…?',
+	'fisica;da fisica;dafisica'
+	,'ph.jpg');
+
+INSERT INTO PARADA VALUES ('USP - Campus 1', 8,
+	'Centro Acadêmico Armando Salles de Oliveira (CAASO)',
+	'O Centro Acadêmico Armando de Salles Oliveira (CAASO) é o centro acadêmico da Universidade de São Paulo em São Carlos, no estado de São Paulo e trata-se de um dos maiores centros acadêmicos da América Latina. É o órgão representativo de todos os alunos de graduação da USP de São Carlos. O CAASO participou ativamente das lutas em defesa da universidade pública e pelo ensino de qualidade no país, mas também se posicionou contra o golpe de Estado que deu início à ditadura militar e passou a ser um polo fundamental de resistência na defesa da redemocratização do país.',
+	'Siga no sentido oposto à saída e depois pela praça até o prédio amarelo, cheio de grafites. A arquibancada fica em frente a um grande local de reuniões. Como ele se chama?',
+	'palquinho'
+	,'ph.jpg');
+
+INSERT INTO PARADA VALUES ('USP - Campus 1', 9,
+	'Biblioteca da EESC',
+	'A Biblioteca Prof. Dr. Sérgio Rodrigues Fontes deu início a suas atividades em 1953 como suporte aos estudos das ciências básicas com acervo inicial de 2.603 volumes divididos entre coleções de periódicos e livros. Seu acesso é livre tanto para alunos da USP quanto a membros da comunidade externa.',
+	'Vá até à livraria, subindo as escadas, contorne o prédio e continue subindo. Um grande professor foi homenageado ao ser o nome deste grande centro de conhecimento e saberes. Quem é ele?',
+	'sergio rodrigues fontes;sergiorodriguesfontes;dr sergio rodrigues fontes;drsergiorodriguesfontes'
+	,'ph.jpg');
+
+INSERT INTO PARADA VALUES ('USP - Campus 1', 10,
+	'Instituto de Ciências Matemáticas e de Computação (ICMC)',
+	'Fundado em 1971, o Instituto de Ciências Matemáticas e de Computação (ICMC) é considerado um dos mais importantes centros de formação nas áreas de matemática, matemática aplicada, computação e estatística. Há oito cursos ministrados pelo instituto: Licenciatura em Matemática, Bacharelado em Matemática, Bacharelado em Matemática Aplicada e Computação Científica, Bacharelado em Ciência da Computação, Bacharelado em Sistemas de Informação, Bacharelado em Estatística, Engenharia da Computação e Licenciatura em Ciências Exatas. As pesquisas nessas áreas são reconhecidas mundialmente através das publicações em periódicos especializados e da participação de seus membros em eventos científicos e projetos de cooperação com instituições estrangeiras. Os grupos de pesquisa recebem apoio de agências de fomento brasileiras, bem como de instituições internacionais.',
+	'Atravesse a rua, suba as escadas e siga reto. Apesar de ser um instituto sobre matemática e computação, ele tem uma grande preocupação com o meio ambiente. Ele tem um museu aberto chamado…?',
+	'fauna e flora;museu da fauna e flora;faunaeflora;museudafaunaeflora'
+	,'ph.jpg');
+
+INSERT INTO PARADA VALUES ('USP - Campus 1', 11,
+	'Teorema da Gaveta',
+	'Achille Bassi é o nome do fundador do Departamento de Matemática da USP de São Carlos. Sua área de pesquisa era Topologia Combinatória. Segundo seus alunos, suas aulas eram temperadas com um forte sotaque italiano e eram sempre muito intensas. Ele também gostava muito de astronomia. Contam-se muitas histórias sobre ele. Uma dessas histórias é um teorema que ele demonstrou e que ficou conhecida como Teorema da Gaveta.',
+	'Siga reto em direção à rotatória e continue andando à direita. O mosaico de azulejos veio de uma anedota interessante. Com que professor ela aconteceu?',
+	'achille bassi;achillebassi'
+	,'ph.jpg');
+
+INSERT INTO PARADA VALUES ('USP - Campus 1', 12,
+	'Edifício E-1 (EESC)',
+	'No edifício E-1 está situado o corpo administrativo e diretoria da EESC. É uma obra arquitetônica que respeita os Cinco Pontos da Nova Arquitetura estabelecidos pelo arquiteto francês Le Corbusier: planta livre, fachada livre, pilotis, terraço jardim e janelas em fita. A origem do nome E-1 se dá pois era planejado a construção de outros prédios desse mesmo tipo, que se chamariam E-2, E-3 e assim por diante, mas esse plano nunca foi concretizado.',
+	'Seguindo pela rua, você vai ver novamente a praça do CAASO. Mas, desta vez, siga para o lado esquerdo. A escultura no centro dessa praça homenageia uma deusa romana. Quem foi seu escultor?',
+	'antonio galvao;antoniogalvao'
+	,'ph.jpg');
+
+INSERT INTO PARADA VALUES ('USP - Campus 1', 13,
+	'Portal Décadas (EESC)',
+	'O Portal Décadas da EESC funciona como um indicador temporal: cada arco representa a passagem de dez anos, começando pela fundação da EESC. Ele é continuamente atualizado.',
+	'Passando por baixo do prédio, não vai ser difícil encontrar vários arcos vermelhos. Em que ano será aberta a Cápsula do Tempo da EESC?',
+	'2053'
+	,'ph.jpg');
+
+INSERT INTO PARADA VALUES ('USP - Campus 1', 14,
+	'Instituto de Física de São Carlos (IFSC)',
+	'O Instituto de Física de São Carlos (IFSC) destaca-se pela sua experiência no ensino e na pesquisa e é reconhecido pelo alto nível científico e pela excelente infraestrutura de seus laboratórios e sua biblioteca. O IFSC oferece os cursos de bacharelado em Física, Ciências Físicas e Biomoleculares e Física Computacional, além do curso interunidades de licenciatura em Ciências Exatas. Todos os cursos, com duração mínima de quatro anos, aliam uma sólida formação em Física aos conhecimentos nas áreas específicas dos cursos. Seus egressos têm atuado tanto na área de pesquisa básica como aplicada junto a centros de pesquisa e indústria.',
+	'Passe por todos os arcos e continue até encontrar uma outra estátua. Um deles imita a Estátua da Liberdade, mas o outro segura outra coisa. O que ela segura?',
+	'atomo;molecula;quimica'
+	,'ph.jpg');
+
+INSERT INTO PARADA VALUES ('USP - Campus 1', 15,
+	'Observatório Astronômico',
+	'Com o objetivo de difundir temas ligados à Astronomia e estimular a observação e os estudos dos fenômenos astronômicos, o CDCC mantém o Setor de Astronomia, cuja infraestrutura conta com auditório, salas de instrumentação e os seguintes telescópios: Refrator Grubb 200/3000, Refrator Zeiss 100/1000, Refrator Newtoniano-Dobsoniano 254/1130 e Refrator Zeiss 63/840). O Observatório promove visitas monitoradas, palestras e cursos, além de orientar trabalhos científicos.',
+	'Contorne o prédio do IFSC e siga na direção de Saturno e, depois, Júpiter. Antes da saída, vire à direita. Você está vendo o monumento com os arcos coloridos? O que significa o arco vermelho?',
+	'solsticio de verao;solsticiodeverao'
+	,'ph.jpg');
+
+COMMIT;
